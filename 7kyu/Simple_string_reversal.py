bb78cfa720282e22c21bc93f18e12a8fdaa09a66
andrey <rodandr13@gmail.com>
2022-11-02 15:38:41 +0300

    ver. 2

--- 7kyu/Simple_string_reversal.py
+++ 7kyu/Simple_string_reversal.py
@@ -1,9 +1,16 @@
+"""
 def solve(s):
     result = [char for char in s[::-1] if char != ' ']
     for i in range(len(s)):
         if s[i] == ' ':
             result.insert(i, ' ')
     return ''.join(result)
+"""
+
+
+def solve(s):
+    result = [char for char in s if char != ' ']
+    return ''.join(c if c == ' ' else result.pop() for c in s)
 
 
 assert solve("codewars") == "srawedoc"
