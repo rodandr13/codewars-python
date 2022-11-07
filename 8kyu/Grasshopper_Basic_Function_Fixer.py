1f0a7b21c6ce5f43158613fe5b5902623701d922
andrey <rodandr13@gmail.com>
2022-11-07 19:01:46 +0300

    done Fix the function

---
+++ 8kyu/Grasshopper_Basic_Function_Fixer.py
@@ -0,0 +1,14 @@
+"""
+I created this function to add five to any number that was passed in to it and return the new value.
+It doesn't throw any errors but it returns the wrong number.
+"""
+
+
+def add_five(num):
+    total = int(num) + 5
+    return total
+
+
+assert add_five(5) == 10
+assert add_five(0) == 5
+assert add_five(-5) == 0
