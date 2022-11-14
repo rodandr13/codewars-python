f3a9ac84d424ecc5b7777b8709323ea040075657
andrey <rodandr13@gmail.com>
2022-11-15 02:35:42 +0300

    done: Spoonerize Me

---
+++ 7kyu/Spoonerize_Me.py
@@ -0,0 +1,19 @@
+"""
+A spoonerism is a spoken phrase in which the first letters of two of the
+words are swapped around, often with amusing results.
+
+In its most basic form a spoonerism is a two word phrase in which only the
+first letters of each word are swapped:
+
+"not picking" --> "pot nicking"
+"""
+
+
+def spoonerize(words):
+    words = words.split()
+    return words[1][:1] + words[0][1:] + " " + words[0][:1] + words[1][1:]
+
+
+assert spoonerize("nit picking") == "pit nicking"
+assert spoonerize("wedding bells") == "bedding wells"
+assert spoonerize("jelly beans") == "belly jeans"
