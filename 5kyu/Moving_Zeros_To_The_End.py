6eec7d5f8736b9fcff3be4e4998d276148b4b841
andrey <rodandr13@gmail.com>
2022-11-15 12:17:38 +0300

    feature: v.2

--- 5kyu/Moving_Zeros_To_The_End.py
+++ 5kyu/Moving_Zeros_To_The_End.py
@@ -4,6 +4,11 @@ preserving the order of the other elements.
 move_zeros([1, 0, 1, 2, 0, 1, 3]) # returns [1, 1, 2, 1, 3, 0, 0]
 """
 
+"""
+def move_zeros(array):
+    return sorted(array, key=lambda x: x==0 and type(x) is not bool)
+"""
+
 
 def move_zeros(lst):
     result = []
