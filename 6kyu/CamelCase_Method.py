1a01e806e310b64542ef0724c9cbef1271e26bd7
andrey <rodandr13@gmail.com>
2022-11-15 14:46:33 +0300

    done: CamelCase Method

---
+++ 6kyu/CamelCase_Method.py
@@ -0,0 +1,18 @@
+"""
+Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or
+camelCase in Java) for strings. All words must have their first letter
+capitalized without spaces.
+
+For instance:
+camelcase("hello case") => HelloCase
+camelcase("camel case word") => CamelCaseWord
+"""
+
+
+def camel_case(string):
+    return "".join(map(str.title, string.split()))
+
+
+assert camel_case("test case") == "TestCase"
+assert camel_case("camel case method") == "CamelCaseMethod"
+assert camel_case("say hello ") == "SayHello"
