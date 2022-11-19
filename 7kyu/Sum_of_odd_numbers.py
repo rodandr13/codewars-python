e697ea0c4f87276faa0ec927eb8c475b26703cf8
andrey <rodandr13@gmail.com>
2022-11-19 19:54:05 +0300

    refactoring: add v.2

--- 7kyu/Sum_of_odd_numbers.py
+++ 7kyu/Sum_of_odd_numbers.py
@@ -10,6 +10,10 @@ Given the triangle of consecutive odd numbers:
 Calculate the sum of the numbers in the nth row of this triangle (starting
 at index 1) e.g.: (Input --> Output)
 """
+"""
+def row_sum_odd_numbers(n):
+    return n ** 3
+"""
 
 
 def row_sum_odd_numbers(n):
