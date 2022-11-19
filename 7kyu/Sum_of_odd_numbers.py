f7b3fc8e69970037a364e03281669a89b3d12a66
andrey <rodandr13@gmail.com>
2022-11-19 19:51:49 +0300

    done: Sum of odd numbers

---
+++ 7kyu/Sum_of_odd_numbers.py
@@ -0,0 +1,21 @@
+"""
+Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+Calculate the sum of the numbers in the nth row of this triangle (starting
+at index 1) e.g.: (Input --> Output)
+"""
+
+
+def row_sum_odd_numbers(n):
+    return n * n * n
+
+
+assert row_sum_odd_numbers(1) == 1
+assert row_sum_odd_numbers(2) == 8
+assert row_sum_odd_numbers(13) == 2197
