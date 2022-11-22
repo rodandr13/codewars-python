068905caf42825cd3eca19911d9292339832ea84
andrey <rodandr13@gmail.com>
2022-11-22 19:19:24 +0300

    done: Find Maximum and Minimum Values of a List

---
+++ 8kyu/Find_Maximum_and_Minimum_Values_of_a_List.py
@@ -0,0 +1,14 @@
+"""
+Your task is to make two functions ( max and min, or maximum and minimum, etc.,
+depending on the language ) that receive a list of integers as input,
+and return the largest and lowest number in that list, respectively.
+"""
+
+
+def minimum(arr):
+    return min(arr)
+
+
+def maximum(arr):
+    return max(arr)
+
