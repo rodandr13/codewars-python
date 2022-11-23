7715277ff861ed2d2beae9134ecff585175f728f
andrey <rodandr13@gmail.com>
2022-11-23 22:11:33 +0300

    done: Opposite number

---
+++ 8kyu/Opposite_number.py
@@ -0,0 +1,12 @@
+"""
+Very simple, given an integer or a floating-point number, find its opposite.
+"""
+
+
+def opposite(number):
+    return number * -1
+
+
+assert opposite(1) == -1
+assert opposite(25.6) == -25.6
+assert opposite(0) == 0
