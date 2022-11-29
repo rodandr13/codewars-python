97b5173e621e0af6831a875638d0ce11a480fa8f
andrey <rodandr13@gmail.com>
2022-11-30 00:07:46 +0300

    ver. 2

--- 7kyu/How_many_points_did_the_teams_from_Los_Angeles_score.py
+++ 7kyu/How_many_points_did_the_teams_from_Los_Angeles_score.py
@@ -1,6 +1,12 @@
 """
 https://www.codewars.com/kata/580559b17ab3396c58000abb
 """
+"""
+import re
+def get_los_angeles_points(results):
+    pattern = re.compile('Los Angeles [A-Z][a-z]+')
+    return sum(int(k[1].split(':')[0]) for k in results if pattern.fullmatch(k[0]))
+"""
 
 
 def get_los_angeles_points(results):
