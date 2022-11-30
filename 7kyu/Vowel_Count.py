1c788e008c20db397927f44dabbad1c950859cee
andrey <rodandr13@gmail.com>
2022-11-30 23:18:31 +0300

    add ver. 2

--- 7kyu/Vowel_Count.py
+++ 7kyu/Vowel_Count.py
@@ -5,6 +5,14 @@ We will consider a, e, i, o, u as vowels for this Kata (but not y).
 
 The input string will only consist of lower case letters and/or spaces.
 """
+"""
+def getCount(inputStr):
+    return sum(1 for let in inputStr if let in "aeiouAEIOU")
+"""
+"""
+def getCount(s):
+    return sum(c in 'aeiou' for c in s)
+"""
 
 
 def get_count(sentence):
