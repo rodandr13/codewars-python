ba9bdf9cbee551e06ddf06bc9639c3989f12c18b
andrey <rodandr13@gmail.com>
2022-12-09 19:45:35 +0300

    add ver.2

--- 8kyu/Double_Char.py
+++ 8kyu/Double_Char.py
@@ -2,6 +2,10 @@
 Given a string, you have to return a string in which each character
 (case-sensitive) is repeated once.
 """
+"""
+def double_char(s):
+    return ''.join(c * 2 for c in s)
+"""
 
 
 def double_char(s):
