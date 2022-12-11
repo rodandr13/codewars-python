a73bf2872797155a73f781c6da92eeaa531a7f44
andrey <rodandr13@gmail.com>
2022-12-11 20:59:57 +0300

    ver.2

--- 7kyu/Deutschstunde.py
+++ 7kyu/Deutschstunde.py
@@ -8,6 +8,11 @@ each noun containing 2/3 vowels has the article "die"
 each noun containing more than 3 vowels has the article "der"
 Caution: Vowels are "a,e,i,o,u". Umlaute (ä ö ü) are also being counted!
 """
+"""
+def der_die_das(wort):
+    a = sum(x in "aAeEiIoOuUäöü" for x in wort)
+    return f'{"das" if a < 2 else "die" if a < 4 else "der"} {wort}'
+"""
 
 
 def der_die_das(wort):
