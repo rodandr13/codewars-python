a37f7b3feed52fb2a184dee5faf53b90c8c6b092
andrey <rodandr13@gmail.com>
2022-12-13 21:50:48 +0300

    done: Sum a list but ignore any duplicates

---
+++ 7kyu/Sum_a_list_but_ignore_any_duplicates.py
@@ -0,0 +1,10 @@
+"""
+Please write a function that sums a list, but ignores any duplicate items
+in the list.
+
+For instance, for the list [3, 4, 3, 6] , the function should return 10.
+"""
+
+
+def sum_no_duplicates(l):
+    return sum([x for x in l if l.count(x) == 1])
