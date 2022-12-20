0d72622105358b1a2ab9f3f1c759547030760ee0
andrey <rodandr13@gmail.com>
2022-12-20 18:41:10 +0300

    вщdo

---
+++ 8kyu/altERnaTIng_cAsE_ALTerNAtiNG_CaSe.py
@@ -0,0 +1,10 @@
+"""
+Define String.prototype.toAlternatingCase (or a similar function/method such
+as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected
+language; see the initial solution for details) such that each lowercase
+letter becomes uppercase and each uppercase letter becomes lowercase.
+"""
+
+
+def to_alternating_case(string):
+    return string.swapcase()
