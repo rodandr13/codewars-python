4519b070886dce2c73e4b11fe30fb8f0b074c642
andrey <rodandr13@gmail.com>
2022-12-28 21:46:35 +0300

    add ver.2

--- 8kyu/Beginner_Reduce_but_Grow.py
+++ 8kyu/Beginner_Reduce_but_Grow.py
@@ -1,10 +1,11 @@
 from functools import reduce
+
 """
 def grow(arr):
-	product = 1
-	for i in arr:
-		product *= i
-	return product
+    product = 1
+    for i in arr:
+        product *= i
+    return product
 """
 
 
