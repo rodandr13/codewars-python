452df9d46fe5bb9f86b663532d8cadd60fb964f3
andrey <rodandr13@gmail.com>
2022-12-28 21:50:07 +0300

    edit ver.2

--- 8kyu/Localize_The_Barycenter_of_a_Triangle.py
+++ 8kyu/Localize_The_Barycenter_of_a_Triangle.py
@@ -13,8 +13,8 @@ This is how our asked function should work: the result of the coordinates
 should be expressed up to four decimals, (rounded result).
 """
 """
-def bar_triang(a, b, c):
-    return [round(sum(x)/3.0, 4) for x in zip(a, b, c)]
+def bar_triang(point_a, point_b, point_c): 
+    return [round(sum(x) / 3, 4) for x in zip(point_a, point_b, point_c)]
 """
 
 
