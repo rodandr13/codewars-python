18dc44315d54b80b02de99dc5b341b4fd0cfddde
andrey <rodandr13@gmail.com>
2023-01-02 23:07:04 +0300

    done: Rock Paper Scissors!

---
+++ 8kyu/Rock_Paper_Scissors.py
@@ -0,0 +1,28 @@
+"""
+Let's play! You have to return which player won! In case of a draw return Draw!.
+
+Examples(Input1, Input2 --> Output):
+
+"scissors", "paper" --> "Player 1 won!"
+"scissors", "rock" --> "Player 2 won!"
+"paper", "paper" --> "Draw!"
+"""
+"""
+def rps(p1, p2):
+    hand = {'rock':0, 'paper':1, 'scissors':2}
+    results = ['Draw!', 'Player 1 won!', 'Player 2 won!']
+    return results[hand[p1] - hand[p2]]
+"""
+
+
+def rps(p1, p2):
+    if p1 == p2:
+        return "Draw!"
+    if p1 == "scissors" and p2 == "paper":
+        return "Player 1 won!"
+    elif p1 == "rock" and p2 == "scissors":
+        return "Player 1 won!"
+    elif p1 == "paper" and p2 == "rock":
+        return "Player 1 won!"
+    else:
+        return "Player 2 won!"
