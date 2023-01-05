d9cfa634f40e719462fad1ec5eaaa5c3eee7a2d3
andrey <rodandr13@gmail.com>
2023-01-05 22:58:09 +0300

    done: Oh, so you like programming? Name all of the keywords!

---
+++ 7kyu/Oh_so_you_like_programming_Name_all_of_the_keywords.py
@@ -0,0 +1,21 @@
+"""
+Yup, fill the provided set with the keywords of your language.
+
+The test provides the number of needed keywords, and the error messages contain
+hints if you need help.
+
+Details:
+Keywords are the words put aside as instructions for the compiler. For example
+(use these to get started):
+if True nonlocal
+If your language has contextual or "soft" keywords, you won't find them here.
+Only keywords that are always recognized are included.
+The example test case will only check your list length.
+The keywords should be strings.
+"""
+
+
+import keyword
+
+keywords = set(keyword.kwlist)
+
