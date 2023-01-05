19f4656598fdb1406cc9e55051f385d803c45a34
andrey <rodandr13@gmail.com>
2023-01-05 21:01:14 +0300

    done: Simple Fun #176: Reverse Letter

---
+++ 7kyu/Simple_Fun_176_Reverse_Letter.py
@@ -0,0 +1,19 @@
+"""
+Task
+Given a string str, reverse it and omit all non-alphabetic characters.
+
+Example
+For str = "krishan", the output should be "nahsirk".
+
+For str = "ultr53o?n", the output should be "nortlu".
+
+Input/Output
+[input] string str
+A string consists of lowercase latin letters, digits and symbols.
+
+[output] a string
+"""
+
+
+def reverse_letter(string):
+    return "".join([x for x in string[::-1] if x.isalpha()])
