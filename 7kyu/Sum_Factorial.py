a3b643f1a5de021dd475675701b8fa46479276ad
andrey <rodandr13@gmail.com>
2023-01-06 19:24:27 +0300

    done: Sum Factorial

---
+++ 7kyu/Sum_Factorial.py
@@ -0,0 +1,31 @@
+"""
+Factorials are often used in probability and are used as an introductory
+problem for looping constructs. In this kata you will be summing together
+multiple factorials.
+
+Here are a few examples of factorials:
+
+4 Factorial = 4! = 4 * 3 * 2 * 1 = 24
+
+6 Factorial = 6! = 6 * 5 * 4 * 3 * 2 * 1 = 720
+In this kata you will be given a list of values that you must first find the
+factorial, and then return their sum.
+
+For example if you are passed the list [4, 6] the equivalent mathematical
+expression would be 4! + 6! which would equal 744.
+"""
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    elif n < 0:
+        return None
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+
+def sum_factorial(lst):
+    return sum([factorial(x) for x in lst])
