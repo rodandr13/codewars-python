cda1d8d4541afd4a5b83ab603bd154a7838f1e38
andrey <rodandr13@gmail.com>
2023-01-07 21:34:24 +0300

    add ver2

--- 7kyu/Most_digits.py
+++ 7kyu/Most_digits.py
@@ -4,6 +4,10 @@ Find the number with the most digits.
 If two numbers in the argument array have the same number of digits,
 return the first one in the array.
 """
+"""
+def find_longest(arr):
+    return max(arr, key=lambda x: len(str(x)))
+"""
 
 
 def find_longest(arr):
