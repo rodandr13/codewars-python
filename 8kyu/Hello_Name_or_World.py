341523acda9037d43d17f969f33d352122473884
andrey <rodandr13@gmail.com>
2023-01-07 23:47:07 +0300

    done: Hello, Name or World!

---
+++ 8kyu/Hello_Name_or_World.py
@@ -0,0 +1,20 @@
+"""
+Define a method hello that returns "Hello, Name!" to a given name,
+or says Hello, World! if name is not given (or passed as an empty String).
+
+Assuming that name is a String and it checks for user typos to return a name
+with a first capital letter (Xxxx).
+
+Examples:
+
+* With `name` = "john"  => return "Hello, John!"
+* With `name` = "aliCE" => return "Hello, Alice!"
+* With `name` not given
+  or `name` = ""        => return "Hello, World!"
+"""
+
+
+def hello(name="World"):
+    if not name:
+        return "Hello, World!"
+    return f"Hello, {name.title()}!"
