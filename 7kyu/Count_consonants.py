e5e2dea117fa7943d38f0e12a0016f1fac4c1d2b
andrey <rodandr13@gmail.com>
2023-01-07 21:21:07 +0300

    add ver 2

--- 7kyu/Count_consonants.py
+++ 7kyu/Count_consonants.py
@@ -6,6 +6,10 @@ of consonants in the string.
 Consonants are all letters used to write English excluding
 the vowels a, e, i, o, u.
 """
+"""
+def consonant_count(str):
+    return sum(1 for c in str if c.isalpha() and c.lower() not in "aeiou")
+"""
 
 
 def consonant_count(s):
