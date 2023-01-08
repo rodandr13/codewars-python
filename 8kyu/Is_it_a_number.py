89aa34fc9c8eebbd86f00b30967f4e47f04ce9dc
andrey <rodandr13@gmail.com>
2023-01-08 23:23:45 +0300

    add ver 2

--- 8kyu/Is_it_a_number.py
+++ 8kyu/Is_it_a_number.py
@@ -2,6 +2,14 @@
 Given a string s, write a method (function) that will return true if its a
 valid single integer or floating number or false if its not.
 """
+"""
+def isDigit(string):
+    try:
+        float(string)
+        return True
+    except:
+        return False
+"""
 
 
 def isDigit(string):
