2702d4c052eb51ac5cb121f0b2fcebd80c025d04
andrey <rodandr13@gmail.com>
2023-01-08 23:27:00 +0300

    done: Lario and Muigi Pipe Problem

---
+++ 8kyu/Lario_and_Muigi_Pipe_Problem.py
@@ -0,0 +1,15 @@
+"""
+Looks like some hoodlum plumber and his brother has been running around
+and damaging your stages again.
+
+The pipes connecting your level's stages together need to be fixed before
+you receive any more complaints.
+
+Pipes list is correct when each pipe after the first index is greater (+1)
+than the previous one, and that there is no duplicates.
+"""
+
+
+def pipe_fix(nums):
+    nums.sort()
+    return [x for x in range(nums[0], nums[-1] + 1)]
