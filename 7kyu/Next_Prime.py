fc6eb12807f3b6994a4e4ea8de5fe2a672451ff7
andrey <rodandr13@gmail.com>
2023-01-09 19:42:29 +0300

    done: Next Prime

---
+++ 7kyu/Next_Prime.py
@@ -0,0 +1,38 @@
+"""
+
+Get the next prime number!
+
+You will get a numbern (>= 0) and your task is to find the next prime number.
+
+Make sure to optimize your code: there will numbers tested up to about 10^12.
+"""
+
+
+import math
+
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+
+    for i in range(5, int(math.sqrt(n) + 1), 6):
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+    return True
+
+
+def next_prime(n):
+    if n <= 1:
+        return 2
+    prime = n
+    found = False
+    while not found:
+        prime = prime + 1
+        if is_prime(prime):
+            found = True
+    return prime
