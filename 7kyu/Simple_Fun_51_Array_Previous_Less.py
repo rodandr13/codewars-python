00ffcd75c8aa200f8816c480310dc1141d4dcfb1
andrey <rodandr13@gmail.com>
2023-01-09 19:26:09 +0300

    add ver 2

--- 7kyu/Simple_Fun_51_Array_Previous_Less.py
+++ 7kyu/Simple_Fun_51_Array_Previous_Less.py
@@ -1,3 +1,29 @@
+"""
+Task
+Given array of integers, for each position i, search among the previous
+positions for the last (from the left) position that contains a smaller
+value. Store this value at position i in the answer.
+If no such value can be found, store -1 instead.
+
+Example
+For items = [3, 5, 2, 4, 5], the output should be [-1, 3, -1, 2, 4].
+"""
+"""
+def array_previous_less(arr):
+    out = []
+
+    for i, n in enumerate(arr):
+        x = -1
+        for m in arr[:i][::-1]:
+            if m < n:
+                x = m
+                break
+        out.append(x)
+
+    return out
+"""
+
+
 def array_previous_less(arr):
     result = [-1]
     for i in range(1, len(arr)):
