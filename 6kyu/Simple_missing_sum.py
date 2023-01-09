cecf753d53c3319a6b6019f1fe4985c18b3df8a3
andrey <rodandr13@gmail.com>
2023-01-09 21:13:22 +0300

    done: Simple missing sum

---
+++ 6kyu/Simple_missing_sum.py
@@ -0,0 +1,21 @@
+"""
+In this Kata, we will calculate the minimum positive number that is not
+a possible sum from a list of positive integers.
+
+solve([1,2,8,7]) = 4 => we can get 1, 2, 3 (from 1+2), but we cannot get 4. 4
+is the minimum number not possible from the list.
+solve([4,1,2,3,12]) = 11. We can get 1, 2, 3, 4, 4+1=5,
+4+2=6,4+3=7,4+3+1=8,4+3+2=9,4+3+2+1=10. But not 11.
+solve([2,3,2,3,4,2,12,3]) = 1. We cannot get 1.
+"""
+
+
+def solve(arr):
+    arr.sort()
+    result = 1
+    for i in range(len(arr)):
+        if result >= arr[i]:
+            result += arr[i]
+        else:
+            break
+    return result
