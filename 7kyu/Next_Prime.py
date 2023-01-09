95696ddcc28c0320735fe95854b74dc212db3e41
andrey <rodandr13@gmail.com>
2023-01-09 19:44:00 +0300

    add ver 3

--- 7kyu/Next_Prime.py
+++ 7kyu/Next_Prime.py
@@ -7,7 +7,25 @@ You will get a numbern (>= 0) and your task is to find the next prime number.
 Make sure to optimize your code: there will numbers tested up to about 10^12.
 """
 """
+ver2
 from gmpy2 import next_prime
+"""
+"""
+ver3
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def next_prime(n):
+    n += 1
+    while not is_prime(n):
+        n += 1
+    return n
 """
 
 import math
