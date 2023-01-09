09a791f9861675523205bf36454063b7459447ae
andrey <rodandr13@gmail.com>
2023-01-09 19:42:59 +0300

    add ver 2

--- 7kyu/Next_Prime.py
+++ 7kyu/Next_Prime.py
@@ -6,7 +6,9 @@ You will get a numbern (>= 0) and your task is to find the next prime number.
 
 Make sure to optimize your code: there will numbers tested up to about 10^12.
 """
-
+"""
+from gmpy2 import next_prime
+"""
 
 import math
 
