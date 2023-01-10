0521130dfd61b7a0a4ae24cb7a3e3e614fa7fc30
andrey <rodandr13@gmail.com>
2023-01-10 23:33:34 +0300

    add ver 2

--- 8kyu/Removing_Elements.py
+++ 8kyu/Removing_Elements.py
@@ -7,6 +7,10 @@ Example:
 
 None of the arrays will be empty, so you don't have to worry about that!
 """
+"""
+def remove_every_other(my_list):
+    return my_list[::2]
+"""
 
 
 def remove_every_other(my_list):
