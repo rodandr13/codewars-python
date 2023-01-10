dc3693ea93e785212d62b97b8eadcc55d85b0d9c
andrey <rodandr13@gmail.com>
2023-01-10 23:39:17 +0300

    add ver 2

--- 8kyu/String_cleaning.py
+++ 8kyu/String_cleaning.py
@@ -8,6 +8,12 @@ text and remove all of the numbers. Your program will take in a string and
 clean out all numeric characters, and return a string with spacing and special
 characters ~#$%^&!@*():;"'.,? all intact.
 """
+"""
+def string_clean(s):
+    for d in '0123456789':
+        s = s.replace(d, '')
+    return s
+"""
 
 
 def string_clean(s):
