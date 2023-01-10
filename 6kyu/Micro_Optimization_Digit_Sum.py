ff341ae71bc38da844f7fea53b20f0f4f40f6b72
andrey <rodandr13@gmail.com>
2023-01-10 17:05:32 +0300

    add ver 2

--- 6kyu/Micro_Optimization_Digit_Sum.py
+++ 6kyu/Micro_Optimization_Digit_Sum.py
@@ -15,6 +15,14 @@ def digit_sum(n):
 def digit_sum(n):
     return n if n<9 else n%10 + digit_sum(n//10)
 """
+"""
+def digit_sum(n):
+    return sum(map(int, str(n)))
+"""
+"""
+def digit_sum(n):
+    return sum(int(i) for i in str(n))
+"""
 
 
 def digit_sum(n):
