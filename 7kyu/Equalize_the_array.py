556260ca1035d1ad114cfcc42ade9689f585d5f4
andrey <rodandr13@gmail.com>
2023-01-11 21:18:27 +0300

    done: Equalize the array!

---
+++ 7kyu/Equalize_the_array.py
@@ -0,0 +1,20 @@
+"""
+No description!!!
+
+Input :: [10,20,25,0]
+
+Output :: ["+0", "+10", "+15", "-10"]
+
+Show some love, rank and upvote!
+"""
+
+
+def equalize(arr):
+    result = []
+    for i in range(len(arr)):
+        temp = arr[i] - arr[0]
+        if temp >= 0:
+            result.append(f"+{temp}")
+        else:
+            result.append(f"{temp}")
+    return result
