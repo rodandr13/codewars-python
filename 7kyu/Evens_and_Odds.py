3d91e80616144b01ca2fe4c34f8b1a33758abd58
andrey <rodandr13@gmail.com>
2023-01-11 21:23:25 +0300

    done: Evens and Odds

---
+++ 7kyu/Evens_and_Odds.py
@@ -0,0 +1,12 @@
+"""
+This kata is about converting numbers to
+their binary or hexadecimal representation:
+
+If a number is even, convert it to binary.
+If a number is odd, convert it to hex.
+Numbers will be positive. The hexadecimal string should be lowercased.
+"""
+
+
+def evens_and_odds(n):
+    return bin(n)[2:] if n % 2 == 0 else hex(n)[2:]
