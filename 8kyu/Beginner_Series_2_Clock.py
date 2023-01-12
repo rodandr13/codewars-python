4d23a368ccbaf6f7e9a8ed060bb450eaf6924c50
andrey <rodandr13@gmail.com>
2023-01-12 23:05:05 +0300

    done: Beginner Series #2 Clock

---
+++ 8kyu/Beginner_Series_2_Clock.py
@@ -0,0 +1,17 @@
+"""
+Clock shows h hours, m minutes and s seconds after midnight.
+
+Your task is to write a function which returns the time since
+midnight in milliseconds.
+
+Example:
+h = 0
+m = 1
+s = 1
+
+result = 61000
+"""
+
+
+def past(h, m, s):
+    return ((h * 60 + m) * 60 + s) * 1000
