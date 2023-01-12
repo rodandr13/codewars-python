dc9736a46cddf0e0cd6047b2cb5792fce802ef78
andrey <rodandr13@gmail.com>
2023-01-12 23:01:26 +0300

    add ver 2

--- 8kyu/Filter_out_the_geese.py
+++ 8kyu/Filter_out_the_geese.py
@@ -7,6 +7,10 @@ in your solution:
 
   ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
 """
+"""
+def goose_filter(birds):
+    return [bird for bird in birds if bird not in geese]
+"""
 
 
 geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
