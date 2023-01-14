c89c8ec688c1c51868bf33c572f891b21e1a7316
andrey <rodandr13@gmail.com>
2023-01-14 22:45:21 +0300

    add ver 2

--- 8kyu/Unfinished_Loop_Bug_Fixing_1.py
+++ 8kyu/Unfinished_Loop_Bug_Fixing_1.py
@@ -3,6 +3,10 @@ Unfinished Loop - Bug Fixing #1
 Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug
 in his unfinished for loop!
 """
+"""
+def create_array(n):
+    return [i for i in range(1,n+1)]
+"""
 
 
 def create_array(n):
