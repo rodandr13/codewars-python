afe82c9d1c22373bd005d8101f9e1a99384b2c17
andrey <rodandr13@gmail.com>
2023-01-16 01:19:00 +0300

    add ver 2

--- 7kyu/Sum_of_Triangular_Numbers.py
+++ 7kyu/Sum_of_Triangular_Numbers.py
@@ -5,6 +5,10 @@ the nth Triangular Number.
 Triangular Number: "any of the series of numbers (1, 3, 6, 10, 15, etc.)
 obtained by continued summation of the natural numbers 1, 2, 3, 4, 5, etc."
 """
+"""
+def sum_triangular_numbers(n):
+    return n*(n+1)*(n+2)/6 if n>0 else 0
+"""
 
 
 def sum_triangular_numbers(n):
