cfe2aac15a549f5f2b580ae24ab5a1209a4e187c
andrey <rodandr13@gmail.com>
2023-01-20 23:43:42 +0300

    done: Well of Ideas - Easy Version

---
+++ 8kyu/Well_of_Ideas_Easy_Version.py
@@ -0,0 +1,15 @@
+"""
+For every good kata idea there seem to be quite a few bad ones!
+
+In this kata you need to check the provided array (x) for good ideas 'good'
+and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!',
+if there are more than 2 return 'I smell a series!'. If there are no good
+ideas, as is often the case, return 'Fail!'.
+"""
+
+
+def well(x):
+    count_good = x.count("good")
+    if not count_good:
+        return "Fail!"
+    return "Publish!" if count_good <= 2 else "I smell a series!"
