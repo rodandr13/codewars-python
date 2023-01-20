a0e6bf5be3f67038469d144d48287ff48350aeb6
andrey <rodandr13@gmail.com>
2023-01-20 23:44:27 +0300

    add ver 2

--- 8kyu/Well_of_Ideas_Easy_Version.py
+++ 8kyu/Well_of_Ideas_Easy_Version.py
@@ -6,6 +6,11 @@ and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!',
 if there are more than 2 return 'I smell a series!'. If there are no good
 ideas, as is often the case, return 'Fail!'.
 """
+"""
+def well(x):
+    c = x.count('good')
+    return 'I smell a series!' if c > 2 else 'Publish!' if c else 'Fail!'
+"""
 
 
 def well(x):
