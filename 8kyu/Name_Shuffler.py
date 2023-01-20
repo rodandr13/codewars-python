a496007c68304ae6b1f46e151c7a57370f552721
andrey <rodandr13@gmail.com>
2023-01-20 23:21:49 +0300

    done: Name Shuffler

---
+++ 8kyu/Name_Shuffler.py
@@ -0,0 +1,12 @@
+"""
+Write a function that returns a string in which firstname is swapped with
+last name.
+
+Example(Input --> Output)
+
+"john McClane" --> "McClane john"
+"""
+
+
+def name_shuffler(str_):
+    return " ".join(str_.split()[::-1])
