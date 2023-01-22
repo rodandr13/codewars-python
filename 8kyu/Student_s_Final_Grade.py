db0ee7a06f65a4557cc2b96e45d5d5e03fec0696
andrey <rodandr13@gmail.com>
2023-01-22 23:48:13 +0300

    add ver 2

--- 8kyu/Student_s_Final_Grade.py
+++ 8kyu/Student_s_Final_Grade.py
@@ -29,6 +29,13 @@ Examples(Inputs-->Output):
 55, 0 --> 0
 20, 2 --> 0
 """
+"""
+def final_grade(exam, projects):
+  if exam > 90 or  projects > 10: return 100
+  if exam > 75 and projects >= 5: return 90
+  if exam > 50 and projects >= 2: return 75
+  return 0
+"""
 
 
 def final_grade(exam, projects):
