ce5ffc16197dff404c7528dfa8afb6d13d1e7e67
andrey <rodandr13@gmail.com>
2023-01-22 23:39:35 +0300

    add ver 2

--- 8kyu/101_Dalmatians_squash_the_bugs_not_the_dogs.py
+++ 8kyu/101_Dalmatians_squash_the_bugs_not_the_dogs.py
@@ -10,10 +10,21 @@ be at least 1 dog.
 
 Good luck!
 """
+"""
+def how_many_dalmatians(n):
+    dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"]
+    return dogs[0] if n <= 10 else dogs[1] if n <=50 else dogs[3] if n == 101 else dogs[2]
+
+"""
 
 
 def how_many_dalmatians(number):
-    dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"];
+    dogs = [
+        "Hardly any",
+        "More than a handful!",
+        "Woah that's a lot of dogs!",
+        "101 DALMATIONS!!!"
+    ]
     if number <= 10:
         return dogs[0]
     elif number <= 50:
