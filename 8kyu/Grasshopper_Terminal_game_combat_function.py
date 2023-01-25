0525084abaf5c13e86a9318db7956959c9c97ed8
andrey <rodandr13@gmail.com>
2023-01-25 23:51:32 +0300

    add ver 2

--- 8kyu/Grasshopper_Terminal_game_combat_function.py
+++ 8kyu/Grasshopper_Terminal_game_combat_function.py
@@ -3,6 +3,10 @@ Create a combat function that takes the player's current health and the amount
 of damage recieved, and returns the player's new health. Health can't be less
 than 0.
 """
+"""
+def combat(health, damage):
+    return max(0, health-damage)
+"""
 
 
 def combat(health, damage):
