06a1ee9672d909978eb4090671398d8a59f8c084
andrey <rodandr13@gmail.com>
2023-01-28 23:32:13 +0300

    add ver

--- 7kyu/Truncate_a_string.py
+++ 7kyu/Truncate_a_string.py
@@ -14,6 +14,10 @@ Examples
 ('codewars', 7)  ==>  'code...'
 ('codewars', 2)  ==>  'co...'
 """
+"""
+def truncate_string(s,n):
+    return s if len(s)<=n else s[:n if n<=3 else n-3]+'...'
+"""
 
 
 def truncate_string(s, n):
