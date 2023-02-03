52d5eaf4407a84e4ded57714a1ce7fe831b576d3
andrey <rodandr13@gmail.com>
2023-02-03 23:04:46 +0300

    done: Grasshopper - Terminal Game #1

--- 8kyu/Grasshopper_Terminal_Game_1.py
+++ 8kyu/Grasshopper_Terminal_Game_1.py
@@ -10,6 +10,15 @@ health	100
 damage	5
 experience	0
 """
+"""
+class Hero(object):
+    def __init__(self, name='Hero', pos='00', health=100, dmg=5, exp=0):
+        self.name = name
+        self.position = pos
+        self.health = health
+        self.damage = dmg
+        self.experience = exp
+"""
 
 
 class Hero(object):
