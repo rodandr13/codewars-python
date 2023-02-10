bb150a02ef62003c22bb77f3aebd9b3c67291eea
andrey <rodandr13@gmail.com>
2023-02-10 23:33:41 +0300

    add ver 2

--- 8kyu/Are_arrow_functions_odd.py
+++ 8kyu/Are_arrow_functions_odd.py
@@ -4,6 +4,10 @@ Time to test your basic knowledge in functions! Return the odds from a list:
 [1, 2, 3, 4, 5]  -->  [1, 3, 5]
 [2, 4, 6]        -->  []
 """
+"""
+def odds(values):
+        return [i for i in values if i%2]
+"""
 
 
 odds = lambda x: [a for a in x if a % 2 != 0]
